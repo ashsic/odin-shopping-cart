3a1c0a84871026e847b4d95aafe2955571cf333f
ashsic <99445200+ashsic@users.noreply.github.com>
2024-02-16 10:06:16 -0500

    feat: added react router with basic routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from './components/Homepage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "cart",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
